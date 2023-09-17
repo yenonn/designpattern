709fb754966ff25d5daf2ef2fd9d848939435a26
yenonn <yenonn@gmail.com>
2023-09-17 20:28:12 +0800

    facade

--- patterns/facade.go
+++ patterns/facade.go
@@ -2,6 +2,7 @@ package patterns
 
 import (
 	"log"
+	"strings"
 )
 
 type LengthChecker struct {
@@ -19,21 +20,40 @@ func (l LengthChecker) IsValid(number string) bool {
 	return true
 }
 
-type Service struct {
-	lengthChecker *LengthChecker
+type PrefixValidator struct {
+	prefix string
 }
 
-func NewService(number int) *Service {
-	return &Service{lengthChecker: NewLengthChecker(number)}
+func NewPrefixValidator(prefix string) *PrefixValidator {
+	return &PrefixValidator{prefix: prefix}
+}
+
+func (validator PrefixValidator) IsValid(number string) bool {
+	if strings.HasPrefix(number, validator.prefix) {
+		return true
+	}
+	return false
+}
+
+type Service struct {
+	lengthChecker   *LengthChecker
+	prefixValidator *PrefixValidator
+}
+
+func NewService(number int, prefix string) *Service {
+	return &Service{
+		lengthChecker:   NewLengthChecker(number),
+		prefixValidator: NewPrefixValidator(prefix),
+	}
 }
 
 func (s Service) process(number string) {
-	ok := s.lengthChecker.IsValid(number)
+	ok := s.lengthChecker.IsValid(number) && s.prefixValidator.IsValid(number)
 	log.Print(ok)
 }
 
 func Facade() {
-	service := NewService(10)
-	service.process("0123456789")
+	service := NewService(10, "123-")
+	service.process("123-567890")
 	service.process("123456789")
 }
