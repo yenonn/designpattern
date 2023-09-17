967b6775f960fd2f438370f6f29e3859922b8834
yenonn <yenonn@gmail.com>
2023-09-17 12:06:21 +0800

    chain of responsibility

--- patterns/chainofreponsibility.go
+++ patterns/chainofreponsibility.go
@@ -27,9 +27,25 @@ func (m *MultiplyHandler) SetNextHandler(handler Handler) {
 	m.nextHandler = &handler
 }
 
+type AdditionalHandler struct {
+	nextHandler *Handler
+}
+
+func (a *AdditionalHandler) Handle(request Request) {
+	result := Request{number: request.number + 2}
+	log.Print(result)
+	if a.nextHandler != nil {
+		(*a.nextHandler).Handle(result)
+	}
+}
+
+func (a *AdditionalHandler) SetNextHandler(handler Handler) {
+	a.nextHandler = &handler
+}
+
 func ChainOfResponsibility() {
 	m := MultiplyHandler{}
-	nextHandler := MultiplyHandler{}
+	nextHandler := AdditionalHandler{}
 	m.SetNextHandler(&nextHandler)
 	r := Request{number: 2}
 	m.Handle(r)
