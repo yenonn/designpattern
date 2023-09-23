593fe91a97b18719aaf0cc7b54d1b68bd5e2c7ad
yenonn <yenonn@gmail.com>
2023-09-23 17:45:05 +0800

    main.go

--- main.go
+++ main.go
@@ -6,38 +6,54 @@ import (
 	"github.com/yenonn/learn-patterns/patterns"
 )
 
+func printDivider() {
+	fmt.Println("---")
+}
+
 func main() {
 	fmt.Println("Hello design pattern in Golang!")
+
+	printDivider()
 	fmt.Println("Adapter pattern")
 	patterns.Adapter()
 
+	printDivider()
 	fmt.Println("Bridge pattern")
 	patterns.Bridge()
 
+	printDivider()
 	fmt.Println("Builder pattern")
 	patterns.Builder()
 
+	printDivider()
 	fmt.Println("Chain of resposibilty pattern")
 	patterns.ChainOfResponsibility()
 
+	printDivider()
 	fmt.Println("Composite pattern")
 	patterns.Composite()
 
+	printDivider()
 	fmt.Println("Decorator pattern")
 	patterns.Decorator()
 
+	printDivider()
 	fmt.Println("Decorator2 pattern")
 	patterns.Decorator2()
 
+	printDivider()
 	fmt.Println("Facade pattern")
 	patterns.Facade()
 
+	printDivider()
 	fmt.Println("Memento pattern")
 	patterns.Memento()
 
+	printDivider()
 	fmt.Println("Observer pattern")
 	patterns.Observer()
 
+	printDivider()
 	fmt.Println("Prototype pattern")
 	patterns.Prototype()
 }
